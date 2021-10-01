b5951d294745cdb213234ae3d768c59df1681440
Rahulparu <88625008+Rahulparu@users.noreply.github.com>
2021-10-01 20:42:58 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -14,32 +14,7 @@ Only editer not auther.......😅😅😅
 You are able to contact me:--> @RahulrsParu 📵
 </i>"""    
     
-    HELP_TEXT = """
-<i>
--> Add Me To Any Group And Make Me Admin..😎
--> Add Me To Your Desired Channel..😜
--> Doubt"s contact--> @RahulrsParu ☺️
-</i>
-<b>Bot Commands (Works Only In Groups) :</b>
-    -> <code>/add chat_id</code>
-                OR                  - To Connect A Group With A Channel (Bot Should Be Admin With Full Previlages In Both Group And Channel)
-     <code>/add @Username</code>
-     
-    -> <code>/del chat_id</code>
-                OR                  - To disconnect A Group With A Channel
-     <code>/del @Username</code>
-     
-    -> <code>/delall</code>  - This Command Will Disconnect All Connected Channel With The Group And Deletes All Its File From DB
     
-    -> <code>/settings</code> -  This Command Will Display You A Settings Pannel Instance Which Can Be Used To Tweek Bot's Settings Accordingly
-            -> <code>Channel</code> - Button Will Show You All The Connected Chats With The Group And Will Show Buttons Correspnding To There Order For Furthur Controls
-            
-            -> <code>Filter Types</code> - Button Will Show You The 3 Filter Option Available In Bot... Pressing Each Buttons Will Either Enable or Disable Them And This Will Take Into Action As Soon As You Use Them Without The Need Of A Restart
-            -> <code>Configure</code> - Button Will Helps You To Change No. of Pages/ Buttons Per Page/ Total Result Without Acutally Editing The Repo... Also It Provide Option To Enable/Disable For Showing Invite Link In Each Results
-            
-            -> <code>Status</code> - Button Will Shows The Stats Of Your Channel
-            
-"""
     
     ABOUT_TEXT = """<b>➥ My Name</b> : <code> Auto Filter Bot</code>
     
